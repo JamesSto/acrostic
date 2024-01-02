fb7e3f45b536a33cb3a05db8a8d4d55d28cfe7cd
Jimmy Stoyell <jimmy@Jimmys-Macbook.local>
2024-01-01 17:28:39 -0800

    swipe working, navigation still stupidly slow

--- acrostic/components/PuzzleMain.tsx
+++ acrostic/components/PuzzleMain.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, useWindowDimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { ACROSTIC_SCREEN } from '../constants/NavigationConstants';
@@ -10,6 +10,7 @@ import PuzzleGrid from './PuzzleGrid'
 import PuzzleCluesView from './PuzzleCluesView'
 import { AcrosticPuzzleData, AcrosticSquareData } from '../puzzle_logic/AcrosticPuzzleData';
 import Keyboard from './keyboard/Keyboard'
+import { TabView, SceneMap } from 'react-native-tab-view';
 
 enum PuzzleSection {
     Grid = "Grid",
@@ -18,41 +19,46 @@ enum PuzzleSection {
 
 const PuzzleMain: React.FC<PuzzleMainProps> = ({ navigation }) => {
     let puzzle: AcrosticPuzzleData = parseAcrosticPuzzle(PUZZLE_TEXT);
-    const [selectedSection, setSelectedSection] = useState(PuzzleSection.Grid)
-
-    useEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <SwitchSectionButton selectedSection={selectedSection} setSelectedSection={setSelectedSection} />
-            ),
-        });
-    }, [navigation, selectedSection]);
+    const layout = useWindowDimensions();
 
     const grid = <PuzzleGrid puzzle={puzzle} />;
     const clueView = <PuzzleCluesView puzzle={puzzle} />;
 
+    const renderScene = SceneMap({
+        [PuzzleSection.Grid]: () => grid,
+        [PuzzleSection.Clues]: () => clueView,
+    });
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: PuzzleSection.Grid, title: 'Grid' },
+        { key: PuzzleSection.Clues, title: 'Clues' },
+    ]);
+
+    useEffect(() => {
+        navigation.setOptions({ 
+            headerRight: () => (
+                <Button onPress={() => setIndex(index == 1 ? 0 : 1)} title={"See " + routes[index == 0 ? 1 : 0].key} />
+            ),
+        });
+    }, [navigation, index]);
+
     return <View style={styles.container}>
-        {selectedSection == PuzzleSection.Grid ? grid : clueView}
+        <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            renderTabBar={() => null}
+            initialLayout={{ width: layout.width }}
+        />
         <View style={styles.keyboardContainer}>
             <Keyboard />
         </View>
     </View>;
 }
 
-const SwitchSectionButton: React.FC<SwitchSectionButtonProps> = ({ selectedSection, setSelectedSection }) => {
-    return <Button onPress={
-        () => setSelectedSection(selectedSection == PuzzleSection.Grid ? PuzzleSection.Clues : PuzzleSection.Grid)
-    }
-        title={selectedSection} />;
-}
-
 interface PuzzleMainProps {
     navigation: NativeStackNavigationProp<any, any>;
-}
-
-interface SwitchSectionButtonProps {
-    selectedSection: PuzzleSection,
-    setSelectedSection: React.Dispatch<React.SetStateAction<any>>;
 }
 
 const styles = StyleSheet.create({
